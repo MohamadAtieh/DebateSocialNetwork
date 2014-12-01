77ffe2dd0e24cd49738488ef12db2124b14e7cfd
Mohamad Atieh <atiyaleb@gmail.com>
2014-11-30 23:35:12 -0500

    Update db.php
    :3 by mistake

--- config/db.php
+++ config/db.php
@@ -1,17 +1,13 @@
 <?php
 $host = 'localhost';
-$username = 'root';
-$password = 'asot#8672';
-$db = 'debate';
+$username = '';
+$password = '';
+$db = '';
 
 
 $con = mysqli_connect($host, $username, $password, $db);
 if(mysqli_connect_errno($con)){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-// $host = 'localhost';
-// $username = 'atieh_debate';
-// $password = 'asot#8672';
-// $db = 'atieh_debate';
 
 ?>
